a919973ad3ba26776ba915e24b2c52ed7a56740b
Pico Riley <acloudy@mail.smu.edu>
2013-08-31 17:02:22 -0400

    That was really beautiful. I'm liking this language so far.

--- test.py
+++ test.py
@@ -4,3 +4,16 @@ Triple double quote
 String notation
 """
 print test
+
+test = "NR"
+print test*5
+
+test = "I only want*this part*of the string"
+print test[12:21]
+
+#Writing the fibonacci sequence seems to be really easy:
+a, b = 0, 1
+while b < 1000:
+    print b,
+    a, b = b, a+b
+
